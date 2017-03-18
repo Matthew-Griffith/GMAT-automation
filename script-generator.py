85561688e237dd5dd7b5e70305edbfb54a3a992b
Matthew-Griffith <matt.griff10@gmail.com>
2017-03-18 19:46:51 -0400

    created a new branch for the script generator

---
+++ script-generator.py
@@ -0,0 +1,4 @@
+# this program will be used to incremental change parameters from the template.script 
+# file and save them to their own file. this will allow us to generate gmat script 
+# files over the range of parameters that can be defined below.
+
