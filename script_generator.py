bd2a64e03e9c6b236a0a565108cacd593959b43b
Matthew-Griffith <matt.griff10@gmail.com>
2017-03-19 05:14:17 -0400

    added a function to create floating point ranges

--- script_generator.py
+++ script_generator.py
@@ -6,10 +6,20 @@ files over the range of parameters that can be defined below.'''
 dateRange = ['01 March 2016', '01 June 2016', '01 September 2016', '01 December 2016']
 timeRange = ['00:00:00.000', '12:00:00.000']
 
+def float_range(start, stop, step):
+    '''python built in range() function doesn't work of floating point numbers.
+    this is a simple function that loop saving your changing start value that will be
+    incremented by your step value.
+    note that to store the values in a list you have to use the list()
+    ex: list(float_range(1.0, 2.0, 0.1))'''
+    while start < stop:
+        yield start
+        start += step
+
 # here we describe the range of state vector components for a keplarian state vector.
-semiMajorAxisRange = range(6728.137, 6978.137, 50)  # kilometers
-eccentricityRange = range(0.0, 0.9, 0.1)
-inclinationRange = [28, 51.6]                       # inclination of ksc and ISS
+semiMajorAxisRange = list(float_range(6728.137, 6978.137, 50))  # kilometers
+eccentricityRange = list(float_range(0.0, 0.9, 0.1))
+inclinationRange = [28, 51.6]       # inclination of ksc and ISS
 
 '''below defines the range for separation velocity vector note that the gmat script
 performs this separation using the VNB coordination system where...
@@ -22,12 +32,17 @@ second, second we account of errors that could occur in the magnitdue and pointi
 of the combined satellites.'''
 
 # here we set the range of the orientations using two angles
-angleFromNormalAxis = range(0, 180, 30) # in degrees
-angleFromVelAxis = range(0, 360, 30)    # in degrees
+angleFromNormalAxis = range(0, 180, 30)         # in degrees
+angleFromVelAxis = range(0, 360, 30)            # in degrees
 # from here we set the variations of the velocity magnitude
-sepVelMag = range(0.06, 0.1, 0.01)      # in m/s
+sepVelMag = list(float_range(0.06, 0.1, 0.01))  # in m/s
 # next we can have the pointing accurcy which is +/- 5 degrees in any direction which
 # will effect the two angles that we defined for the orientation
-pointErrNormal = range(-5, 5, 1)        # in degrees
-pointErrVel = range(-5, 5, 1)           # in degrees
+pointErrNormal = range(-5, 5, 1)                # in degrees
+pointErrVel = range(-5, 5, 1)                   # in degrees
+# here we will create empty lists to hold our VNB values that will appended in for
+# loops that will be written later.
+sepVelRangeV = []
+sepVelRangeN = []
+sepVelRangeB = []
 # next we use these ranges to define the sep velocity vector and convert to VNB
