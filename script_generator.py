ba9b93d66a139099666706febb7d4a7fb8873b5e
Matthew-Griffith <matt.griff10@gmail.com>
2017-03-20 18:09:59 -0400

    script generator almost works just need to fix an error with the filenames of the reports

--- script_generator.py
+++ script_generator.py
@@ -5,8 +5,8 @@ files over the range of parameters that can be defined below.'''
 from math import sin, cos, pi
 
 # here we will define the range of dates and times to test the simulation over.
-dateRange = ["01 Mar 2016", "01 Jun 2016", "01 Sep 2016", "01 Dec 2016"]
-timeRange = ["00:00:00.000", "12:00:00.000"]
+dateRange = ["'01 Mar 2016", "'01 Jun 2016", "'01 Sep 2016", "'01 Dec 2016"]
+timeRange = ["00:00:00.000'", "12:00:00.000'"]
 
 def float_range(start, stop, step):
     '''python built in range() function doesn't work of floating point numbers.
@@ -20,7 +20,7 @@ def float_range(start, stop, step):
 
 # here we describe the range of state vector components for a keplarian state vector.
 semiMajorAxisRange = list(float_range(6728.137, 6978.137, 50))  # kilometers
-eccentricityRange = list(float_range(0.0, 0.9, 0.1))
+eccentricityRange = list(float_range(0.0, 0.5, 0.1))
 inclinationRange = [28, 51.6]       # inclination of ksc and ISS
 
 '''below defines the range for separation velocity vector note that the gmat script
@@ -42,7 +42,7 @@ sepVelMag = list(float_range(6e-05, 10e-05, 1e-05))  # in km/s
 # will effect the two angles that we defined for the orientation
 pointingAccStart = -5 * (pi/180)
 pointingAccStop = 5 * (pi/180)
-pointingAccStep = 1 * (pi/180)
+pointingAccStep = 2.5 * (pi/180)
 pointErrNormal = list(float_range(pointingAccStart, pointingAccStop, pointingAccStep))  # in rad
 pointErrVel = list(float_range(pointingAccStart, pointingAccStop, pointingAccStep))     # in rad
 # here we will create empty lists to hold our VNB values that will appended in for
@@ -83,12 +83,13 @@ pathReport = "C:/Users/mattg/Documents/GitHub/GMAT-automation/GMAT-Reports/"
 semiMajorAxisRange = list(map(str, semiMajorAxisRange))
 eccentricityRange = list(map(str, eccentricityRange))
 inclinationRange = list(map(str, inclinationRange))
-sepVelRangeV = list(map(str, sepVelRangeV))
-sepVelRangeN = list(map(str, sepVelRangeN))
-sepVelRangeB = list(map(str, sepVelRangeB))
+sepVelRangeVStr = list(map(str, sepVelRangeV))
+sepVelRangeNStr = list(map(str, sepVelRangeN))
+sepVelRangeBStr = list(map(str, sepVelRangeB))
 
-# now we will read in the template script into memory.
-template = open("template.script", "r")
+# there is a issue with filename size, so in order to create unique short filenames 
+# we will create a counter and increment it each iteration.
+counter = 0
 
 # now that all of our ranges have been set and the template file is loaded into memory
 # we will create another set of nested for loops like for the separation vector
@@ -102,8 +103,47 @@ for iTime in range(len(timeRange)):
                         # given ranges, the first thing we need is a helpful filename
                         filename = (dateRange[iDate] + "_" + timeRange[iTime] + "_" + semiMajorAxisRange[iSMA] + "_"
                                     + eccentricityRange[iEcc] + "_" + inclinationRange[iInc] + "_"
-                                    + sepVelRangeV[iSepVec] + "_" + sepVelRangeN[iSepVec] + "_"
-                                    + sepVelRangeB[iSepVec])
-                        # now we can use our new file name to create a file with that Name
-                        currentFile = open(filename + ".script", 'w')
-                        
+                                    + sepVelRangeVStr[iSepVec] + "_" + sepVelRangeNStr[iSepVec] + "_"
+                                    + sepVelRangeBStr[iSepVec] + ".csv")
+                        counter += 1
+                        # now we will read in the template script into memory.
+                        template = open("template.script", "r")
+                        currentFile = open("Generated-Scripts/" + str(counter) + ".script", 'w')
+                        for line in template.readlines():
+                            # now we loop through all the lines in our template file and write to the 
+                            # currentFile 
+                            if line.startswith("GMAT Emitter.Epoch ="):
+                                currentFile.write("GMAT Emitter.Epoch = " + dateRange[iDate] + " " + timeRange[iTime] + ";\n")
+                            elif line.startswith("GMAT Detector.Epoch ="):
+                                currentFile.write("GMAT Detector.Epoch = " + dateRange[iDate] + " " + timeRange[iTime] + ";\n")
+                            elif line.startswith("GMAT Emitter.SMA"):
+                                currentFile.write("GMAT Emitter.SMA = " + semiMajorAxisRange[iSMA] + ";\n")
+                            elif line.startswith("GMAT Detector.SMA"):
+                                currentFile.write("GMAT Detector.SMA = " + semiMajorAxisRange[iSMA] + ";\n")
+                            elif line.startswith("GMAT Emitter.ECC"):
+                                currentFile.write("GMAT Emitter.ECC = " + eccentricityRange[iEcc] + ";\n")
+                            elif line.startswith("GMAT Detector.ECC"):
+                                currentFile.write("GMAT Detector.ECC = " + eccentricityRange[iEcc] + ";\n")
+                            elif line.startswith("GMAT Emitter.INC"):
+                                currentFile.write("GMAT Emitter.INC = " + inclinationRange[iInc] + ";\n")
+                            elif line.startswith("GMAT Detector.INC"):
+                                currentFile.write("GMAT Detector.INC = " + inclinationRange[iInc] + ";\n")
+                            elif line.startswith("GMAT EmitterDetatchment.Element1"):
+                                currentFile.write("GMAT EmitterDetatchment.Element1 = " + str(sepVelRangeV[iSepVec]/2) + ";\n")
+                            elif line.startswith("GMAT EmitterDetatchment.Element2"):
+                                currentFile.write("GMAT EmitterDetatchment.Element2 = " + str(sepVelRangeN[iSepVec]/2) + ";\n")
+                            elif line.startswith("GMAT EmitterDetatchment.Element3"):
+                                currentFile.write("GMAT EmitterDetatchment.Element3 = " + str(sepVelRangeB[iSepVec]/2) + ";\n")
+                            elif line.startswith("GMAT DetectorDetachment.Element1"):
+                                currentFile.write("GMAT DetectorDetachment.Element1 = " + str(-1 * sepVelRangeV[iSepVec]/2) + ";\n")
+                            elif line.startswith("GMAT DetectorDetachment.Element2"):
+                                currentFile.write("GMAT DetectorDetachment.Element2 = " + str(-1 * sepVelRangeN[iSepVec]/2) + ";\n")
+                            elif line.startswith("GMAT DetectorDetachment.Element3"):
+                                currentFile.write("GMAT DetectorDetachment.Element3 = " + str(-1 * sepVelRangeB[iSepVec]/2) + ";\n")
+                            elif line.startswith("GMAT SatelliteReports.Filename"):
+                                currentFile.write("GMAT SatelliteReports.Filename = " + pathReport + filename + ";\n")
+                            else:
+                                currentFile.write(line)
+                        # now we need to close the currentFile
+                        currentFile.close()
+template.close()
