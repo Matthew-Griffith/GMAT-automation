7755c59d90cbce880e90d127c3d60daa17d21e4b
Matthew-Griffith <matt.griff10@gmail.com>
2017-03-20 13:18:10 -0400

    started working on the for loops that will write the files, but am running into an issue with the length of the  filename being too long.

--- script_generator.py
+++ script_generator.py
@@ -5,14 +5,14 @@ files over the range of parameters that can be defined below.'''
 from math import sin, cos, pi
 
 # here we will define the range of dates and times to test the simulation over.
-dateRange = ['01 March 2016', '01 June 2016', '01 September 2016', '01 December 2016']
-timeRange = ['00:00:00.000', '12:00:00.000']
+dateRange = ["01 Mar 2016", "01 Jun 2016", "01 Sep 2016", "01 Dec 2016"]
+timeRange = ["00:00:00.000", "12:00:00.000"]
 
 def float_range(start, stop, step):
     '''python built in range() function doesn't work of floating point numbers.
     this is a simple function that loop saving your changing start value that will be
     incremented by your step value.
-    note that to store the values in a list you have to use the list()
+    note that to store the values in a list you have to use list()
     ex: list(float_range(1.0, 2.0, 0.1))'''
     while start < stop:
         yield start
@@ -35,9 +35,9 @@ of the combined satellites.'''
 
 # here we set the range of the orientations using two angles
 angleFromNormalAxis = list(float_range(0, pi, pi/3))         # in rad
-angleFromVelAxis = list(float_range(0, 2*pi, pi/3))           # in rad
+angleFromVelAxis = list(float_range(0, 2*pi, pi/3))          # in rad
 # from here we set the variations of the velocity magnitude
-sepVelMag = list(float_range(6e-05, 1e-04, 1e-05))  # in km/s
+sepVelMag = list(float_range(6e-05, 10e-05, 1e-05))  # in km/s
 # next we can have the pointing accurcy which is +/- 5 degrees in any direction which
 # will effect the two angles that we defined for the orientation
 pointingAccStart = -5 * (pi/180)
@@ -50,12 +50,18 @@ pointErrVel = list(float_range(pointingAccStart, pointingAccStop, pointingAccSte
 sepVelRangeV = []
 sepVelRangeN = []
 sepVelRangeB = []
-# next we use these ranges to define the sep velocity vector and convert to VNB
+'''next we use these ranges to define the sep velocity vector and convert to VNB.
+to do this we will create a for loop for each of the ranges described above and
+nest them within each other.
+do to the many nested loops the var that represents the place in the iteration were
+written to be more descripted.'''
 for iAngleFromNorm in range(len(angleFromNormalAxis)):
     for iAngleFromVel in range(len(angleFromVelAxis)):
         for iSepMag in range(len(sepVelMag)):
             for iPointErrNorm in range(len(pointErrNormal)):
                 for iPointErrVel in range(len(pointErrVel)):
+                    # here V, N and B are temp vars that will be appended on those empty
+                    # lists that were created earlier.
                     V = sepVelMag[iSepMag] * sin(angleFromVelAxis[iAngleFromVel] + 
                         pointErrVel[iPointErrVel]) * cos(angleFromNormalAxis[iAngleFromNorm] + 
                         pointErrNormal[iPointErrNorm])
@@ -67,3 +73,37 @@ for iAngleFromNorm in range(len(angleFromNormalAxis)):
                     sepVelRangeV.append(V)
                     sepVelRangeN.append(N)
                     sepVelRangeB.append(B)
+
+# here we will set the paths to save the GMAT reports and script files
+# here I use an absolute path which makes the running of the scripts easier
+pathScript = "C:/Users/mattg/Documents/GitHub/GMAT-automation/Generated-Scripts/"
+pathReport = "C:/Users/mattg/Documents/GitHub/GMAT-automation/GMAT-Reports/"
+
+# in order to write to the files the ranges need to be converted into strings
+semiMajorAxisRange = list(map(str, semiMajorAxisRange))
+eccentricityRange = list(map(str, eccentricityRange))
+inclinationRange = list(map(str, inclinationRange))
+sepVelRangeV = list(map(str, sepVelRangeV))
+sepVelRangeN = list(map(str, sepVelRangeN))
+sepVelRangeB = list(map(str, sepVelRangeB))
+
+# now we will read in the template script into memory.
+template = open("template.script", "r")
+
+# now that all of our ranges have been set and the template file is loaded into memory
+# we will create another set of nested for loops like for the separation vector
+for iTime in range(len(timeRange)):
+    for iDate in range(len(dateRange)):
+        for iSMA in range(len(semiMajorAxisRange)):
+            for iEcc in range(len(eccentricityRange)):
+                for iInc in range(len(inclinationRange)):
+                    for iSepVec in range(len(sepVelRangeV)):
+                        # now these for loop will track and update our position in their
+                        # given ranges, the first thing we need is a helpful filename
+                        filename = (dateRange[iDate] + "_" + timeRange[iTime] + "_" + semiMajorAxisRange[iSMA] + "_"
+                                    + eccentricityRange[iEcc] + "_" + inclinationRange[iInc] + "_"
+                                    + sepVelRangeV[iSepVec] + "_" + sepVelRangeN[iSepVec] + "_"
+                                    + sepVelRangeB[iSepVec])
+                        # now we can use our new file name to create a file with that Name
+                        currentFile = open(filename + ".script", 'w')
+                        
